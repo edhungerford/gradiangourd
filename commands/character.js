1e4eb80cc93c670becf79a5bf32fb74ddae2d81d
edhungerford <79060679+edhungerford@users.noreply.github.com>
2022-11-12 02:59:24 -0500

    First draft of /character command

---
+++ commands/character.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('character')
+		.setDescription('Finds a character by name.')
+		.addStringOption(option => 
+			option.setName('name')
+			.setDescription('The name of the character')
+			.setRequired(true)),
+	async execute(interaction) {
+		const char = await axios.get('https://gradia.edsite.black/api/characters');
+		const name = interaction.options.getString('name');
+		var results = char.data.filter(character => {
+			return character.name.toUpperCase().startsWith(name.toUpperCase());
+		})
+		if(results.length > 0){
+			console.log(results);
+			const card = {
+				color: 0xca4a00,
+				title: results[0].name,
+				image: {'url': results[0].url},
+				description: results[0].description,
+				fields: [{name: 'Pronouns', value: results[0].pronouns}]
+			}
+			if(results.length > 1) card.footer = {text: "I found more than one character that matched your search parameters. If this isn't the character you were looking for, try being a little more specific!"};
+			interaction.editReply({embeds: [card]});
+		} else {
+			interaction.editReply("Couldn't find a character with that name.");
+		}
+	},
+};
---
+++ index.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Events, Collection, GatewayIntentBits } = require('discord.js');
+
+const bot = new Client({intents: [GatewayIntentBits.Guilds] });
+bot.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		bot.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+ 
+bot.on('ready', function() {
+    console.log('Logged in as %s.\n', bot.user.tag);
+});
+
+bot.login(process.env.DISCORD_TOKEN);
+
+bot.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	
+  const command = interaction.client.commands.get(interaction.commandName);
+	await interaction.deferReply();
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
